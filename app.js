f8c5e4340eb397e8c62830780ac5e4e590723daa
Collin Stedman <kronion@users.noreply.github.com>
2015-04-13 21:47:36 -0400

    Just some more general setup

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
+/* Express */
 var express = require('express');
 var app = express();
-var GithubApi = require('github');
 
+/* App settings */
+app.set('port', process.env.PORT || 8008);
+
+/* Middleware */
+var compression = require('compression');
+
+app.use(compression())
+   .use(express.static(__dirname + '/public'));
+
+/* GitHub API */
+var GithubApi   = require('github');
 var github = new GithubApi({
   version: '3.0.0',
   protocol: 'https'
 });
 
-app.use(express.static(__dirname + '/public'));
+/* Utils */
+if (process.env.NODE_ENV !== 'production') {
+  var clc = require('cli-color');
+}
+
+var parseCommit = function(commits) {
+}
 
 var getCommits = function(data) {
   var results = [];
+  // For each event...
   for (var i = 0; i < data.length; i++) {
     if (data[i].payload && data[i].payload.commits) {
+      // For each commit...
       for (var j = 0; j < data[i].payload.commits.length; j++) {
         var o = {};
         o.email   = data[i].payload.commits[j].author.email;
@@ -24,6 +43,7 @@ var getCommits = function(data) {
   return results;
 }
 
+/* Routes */
 app.get('/', function(req, res) {
   github.events.getFromUser({ user: 'kronion' }, function(err, data) {
     if (err) {
@@ -31,10 +51,13 @@ app.get('/', function(req, res) {
       res.sendStatus(err);
     }
     else {
-      // console.log(data);
       res.send(getCommits(data));
     }
   });
 });
 
-app.listen(8008);
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log("Git Commitment listening on port " + clc.green(app.get('port')));
+}
+app.listen(app.get('port'));
